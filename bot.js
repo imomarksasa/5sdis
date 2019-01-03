b6b6976aedf15e21d8a59da38be30ba71fe96446
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2019-01-04 01:36:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,6 +15,12 @@ client.on('message', message => {
       }
 });
 
+client.on('ready', () => {
+    client.user.setGame(`5sBot`,"http://twitch.tv/y04zgamer")
+    client.user.setStatus("dnd")
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
 client.on('message', message => {
                                 if(!message.channel.guild) return;
                         if (message.content.startsWith('+ping')) {
