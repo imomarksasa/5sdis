c225406035fa8613f80caf85c7be024a44e92c69
imomarksasa <41761073+imomarksasa@users.noreply.github.com>
2019-01-19 20:27:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const moment = require('moment');
 const jimp = require('jimp');
 const Canvas = require('canvas');
- const prefix = "+";
 client.on('ready', () => {
     console.log('I am ready!');
 });
